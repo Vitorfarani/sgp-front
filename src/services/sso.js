2e21df1f7f88273112692f2bc94af85c21f8c0c0
Lucas Mota <lucas.d.mota@gmail.com>
2023-09-12 17:23:22 -0300

    fix: remove CLIENT_SECRET do front

--- src/services/sso.js
+++ src/services/sso.js
@@ -2,29 +2,6 @@
 import { ENV } from "@/constants/ENV";
 import { getRedirectUrl } from "@/utils/helpers/mask";
 
-// export const sendCodeToGov = async (code) => {
-//     // return MOCK_SSO; //mock para tests, TODO: normalizar auth
-//     Object.assign(httpSSO.defaults, {
-//         headers: { authorization: 'Bearer ' + token },
-//     });
-//     let url = `/token`
-//     const deepLink = getRedirectUrl()
-//     const params = []
-//         params.push('grant_type='+ 'authorization_code');
-//         params.push('&client_id='+ ENV.REACT_APP_CLIENT_ID);
-//         params.push('&client_secret='+ ENV.REACT_APP_CLIENT_SECRET);
-//         params.push('&redirect_uri='+ deepLink);
-//         params.push('&code='+ code);
-
-//     return await httpSSO.post(url, encodeURI(params.join('')))
-//         .then(response => {
-//             return Promise.resolve(response.data)
-//         })
-//         .catch(async (error) => {
-//             console.log(error)
-//             return Promise.reject(ApiAlertError(error))
-//         })
-// }
 
 export const logoutGov = async (data) => {
     const deepLink = getRedirectUrl();
