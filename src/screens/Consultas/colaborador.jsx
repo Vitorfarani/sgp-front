d375423eea36b1fb13950c419d03359b117b031d
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-01-09 14:09:43 -0300

    Filtro de setor, ordem crescente e decrescente e limpar filtros

--- src/screens/Consultas/colaborador.jsx
+++ src/screens/Consultas/colaborador.jsx
@@ -1,17 +1,12 @@
 import { Background, HeaderTitle, Section, SelectAsync, Table } from "@/components/index";
-import { useAuth } from "@/utils/context/AuthProvider";
-import { useTheme } from "@/utils/context/ThemeProvider";
 import useTable from "@/utils/hooks/useTable";
 import { useEffect, useState } from "react";
 import { FiCheckCircle, FiEdit, FiPlus, FiTrash } from "react-icons/fi";
-import { useNavigate } from "react-router-dom";
-import { standartResponseApiError } from "@/services/index";
-import { celularMask } from "@/utils/helpers/mask";
-import { listColaboradores } from "@/services/colaborador/colaboradores";
+import { FcClearFilters } from "react-icons/fc";
 import { listConhecimentoByColaborador } from "@/services/consultas/consultas";
-import {listConhecimentoNivels} from "@/services/conhecimento/conhecimentoNivel";
+import { listConhecimentoNivels } from "@/services/conhecimento/conhecimentoNivel";
 import { listConhecimentos } from "@/services/conhecimento/conhecimentos";
-import { listColaboradorConhecimento } from "@/services/colaborador/colaboradorConhecimento";
+import { listSetores } from "@/services/setores";
 import { Col } from "react-bootstrap";
 
 const basefilters = {
@@ -26,15 +21,17 @@ const basefilters = {
 
 
 const columnsFields = [
-  { field: 'colaborador', label: 'Colaborador', enabledOrder: true, style: { width: 100 }},
-  { field: 'nome', label: 'Conhecimento', enabledOrder: false},
-  { field: 'grau', label: 'Nivel', enabledOrder: false},
+  { field: 'colaborador', label: 'Colaborador', enabledOrder: true, style: { width: 100 } },
+  { field: 'nome', label: 'Conhecimento', enabledOrder: true },
+  { field: 'grau', label: 'Nivel', enabledOrder: true },
+  { field: 'sigla', label: 'Setor', enabledOrder: true },
+
 ];
 
 export default function ConsultarColaborador() {
-  const navigate = useNavigate();
-  const { user } = useAuth();
-  const { callGlobalDialog, handleGlobalLoading, callGlobalAlert, callGlobalNotify } = useTheme();
+  const [selectedConhecimento, setSelectedConhecimento] = useState(null);
+  const [selectedNivel, setSelectedNivel] = useState(null);
+  const [selectedSetor, setSelectedSetor] = useState(null);
 
   const {
     rows,
@@ -46,43 +43,74 @@ export default function ConsultarColaborador() {
     resetFilters,
     isEmpty,
   } = useTable(columnsFields, listConhecimentoByColaborador, basefilters, (results) => {
-    // Verifique se results é um objeto antes de fazer a conversão para array
-    if (typeof results !== 'object' || results === null) {
-      console.error('Os resultados recebidos não são um objeto:', results);
+    if (!results || Object.keys(results).length === 0) {
       return [];
     }
-  
+
     const mappedData = [];
-  
-    // Transforme os resultados em uma lista única de colaboradores com seus conhecimentos
-    for (const colaborador of Object.values(results)) {
+
+    for (const [id, colaborador] of Object.entries(results)) {
       for (const conhecimento of colaborador.conhecimento) {
         mappedData.push({
           colaborador: colaborador.colaborador,
           nome: conhecimento.nome,
           grau: conhecimento.grau,
+          sigla: colaborador.setor?.sigla || '',
         });
       }
     }
-  
-    // Filtre os dados com base no valor da pesquisa
-    const filteredData = mappedData.filter((item) =>
-      item.colaborador.toLowerCase().includes(filtersState.search.toLowerCase())
-    );
-  
-    return filteredData;
+
+    let sortedData = [...mappedData];
+
+    if (
+      filtersState.sortedColumn &&
+      filtersState.sortOrder &&
+      sortedData.length > 0 &&
+      sortedData[0][filtersState.sortedColumn]
+    ) {
+      sortedData = sortedData.sort((a, b) => {
+        const fieldA = a[filtersState.sortedColumn];
+        const fieldB = b[filtersState.sortedColumn];
+
+        if (filtersState.sortOrder === 'asc') {
+          return fieldA.localeCompare(fieldB);
+        } else {
+          return fieldB.localeCompare(fieldA);
+        }
+      });
+    }
+
+    return sortedData;
   });
-  
+
+
   useEffect(() => {
-    // Atualize a função de filtro ao alterar o valor de pesquisa
+
     handleChangeFilters('search', basefilters.search);
     load();
   }, [basefilters.search]);
 
-  
+
+  const handleResetFilters = () => {
+    handleChangeFilters('conhecimento_nivel_id', '');
+    handleChangeFilters('conhecimento_id', '');
+    setSelectedConhecimento(null);
+    setSelectedNivel(null);
+    setSelectedSetor(null);
+  };
+
   return (
     <Background>
-      <HeaderTitle title="Consultar Colaboradores"/>
+      <HeaderTitle
+        title="Consultar Colaboradores"
+        optionsButtons={[
+          {
+            label: 'Limpar filtro(s)',
+            icon: FcClearFilters,
+            onClick: handleResetFilters,
+          },
+        ]}
+      />
       <Section>
         <Table
           columns={columns}
@@ -93,31 +121,51 @@ export default function ConsultarColaborador() {
           searchOffiline
           filtersComponentes={
             <>
-            <Col md={3}>
-            <SelectAsync
-              placeholder="Filtrar por Nivel"
-              loadOptions={(search) => listConhecimentoNivels('?search='+search)}
-              getOptionLabel={(option) => option.grau}
+              <Col md={3}>
+                <SelectAsync
+                  placeholder="Filtrar por Nivel"
+                  loadOptions={(search) => listConhecimentoNivels('?search=' + search)}
+                  getOptionLabel={(option) => option.grau}
+                  onChange={(nivel) => {
+                    setSelectedNivel(nivel);
+                    handleChangeFilters('conhecimento_nivel_id', nivel.id);
+                  }}
+                  value={selectedNivel || ''}
+                />
+              </Col>
+              <Col md={3}>
+                <SelectAsync
+                  placeholder="Filtrar por Conhecimento"
+                  loadOptions={(search) => listConhecimentos('?search=' + search)}
+                  getOptionLabel={(option) => option.nome}
+                  onChange={(conhecimento) => {
+                    setSelectedConhecimento(conhecimento);
+                    handleChangeFilters('conhecimento_id', conhecimento.id);
+                  }}
+                  value={selectedConhecimento || ''}
+                />
+              </Col>
+              <Col md={3}>
+                <SelectAsync
+                  placeholder="Filtrar por Setor"
+                  loadOptions={(search) => listSetores('?search=' + search)}
+                  getOptionLabel={(option) => option.sigla}
+                  onChange={(setor) => {
+                    setSelectedSetor(setor);
+                    handleChangeFilters('setor_id', setor.id);
+                  }}
+                  value={selectedSetor || ''}
+                />
+              </Col>
 
-              onChange={(nivel) => handleChangeFilters('conhecimento_nivel_id', nivel.id)} />
-            </Col>
-            <Col md={3}>
-            <SelectAsync
-              placeholder="Filtrar por Conhecimento"
-              loadOptions={(search) => listConhecimentos('?search='+search)}
-              
-              getOptionLabel={(option) => option.nome}
-
-              onChange={(conhecimento) => handleChangeFilters('conhecimento_id', conhecimento.id)} />
-            </Col>
             </>
 
           }
           handleFilters={handleChangeFilters}>
-            
+
         </Table>
       </Section>
     </Background>
-  );
+  );
 }
 
