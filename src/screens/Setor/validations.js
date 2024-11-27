7eee44049681c189a99587a2022e4cdd13d282b5
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-11-26 23:34:39 -0300

    feat: inicio da inserção de mais responsáveis

--- src/screens/Setor/validations.js
+++ src/screens/Setor/validations.js
@@ -6,4 +6,8 @@ export const setorSchema = Yup.object().shape({
   sigla: Yup.string().required( yupRequired('Sigla')),
   responsavel: Yup.object().nullable(),
   setor: Yup.object().nullable(),
+  dataInicio: Yup.date().required('A data de início é obrigatória'),
+  dataFim: Yup
+  .date()
+  .min(Yup.ref('dataInicio'), 'A data de fim deve ser posterior à data de início'),
 });
